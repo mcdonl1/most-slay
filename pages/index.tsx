2ca641375bdd8efe90bc3425e5f952dfdfe85e43
Lucas McDonald <l.mcdonald@medatech.ca>
2022-12-09 22:35:48 -0500

    added simple interface w no functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,27 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Something Something Slay</title>
       </Head>
-      <h1>VOTE FOR MOST SLAY</h1>
-      <p>
-        Vote for which of the two displayed pokemon (or something else) is the
-        most slay
-      </p>
-    </div>
+      <div className={styles.container}>
+        <h1>VOTE FOR MOST SLAY</h1>
+        <p>
+          Vote for which of the two displayed pokemon (or something else) is the
+          most slay
+        </p>
+        <div className="voting-pane">
+          <div className="card">
+            <h2>Candidate 1</h2>
+          </div>
+          <div className="card">
+            <h2>Candidate 2</h2>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
