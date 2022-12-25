1f04bf5ea479e71b6b06a6918858479b96ebd0b3
Lucas McDonald <l.mcdonald@medatech.ca>
2022-12-25 12:27:43 -0500

    sync

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,25 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Something Something Slay</title>
       </Head>
-      <h1>VOTE FOR MOST SLAY</h1>
-      <p>
-        Vote for which of the two displayed pokemon (or something else) is the
-        most slay
-      </p>
-    </div>
+      <div className={styles.container}>
+        <h1>VOTE FOR MOST SLAY</h1>
+        <p>
+          Vote for which of the two displayed pokemon (or something else) is the
+          most slay
+        </p>
+        <div className="voting-pane">
+          <div className="card">
+            <h2>Candidate 1</h2>
+          </div>
+          <div className="card">
+            <h2>Candidate </h2>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
